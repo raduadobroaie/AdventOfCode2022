538802d6a825c03b7b6958ef99bce4f49cc10695
Radu Adobroaie <radu.adobroaie@gmail.com>
2022-12-09 19:47:33 +0000

    Add solution to problem 1 part 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,25 @@
 fun main() {
     val solution1 = solution1()
 
-    println("Max total: " + solution1.maxTotal())
-    println("Elf with max total: " + solution1.elfWithMaxTotal())
+    // Part 1
+    println("Max total: " + solution1.getMaxTotal())
+    println("Elf with max total: " + solution1.getElfWithMaxTotal())
+
+    // Part 2
+    val topThreeTotals = solution1.getTopThreeTotals()
+    println("Top three: $topThreeTotals")
+    println("Top three total calories: " + topThreeTotals.sum())
 }
 
 fun solution1 (): AoC_2022_01 {
     val solution = AoC_2022_01("src/main/kotlin/data/inputFile1")
+
+    solution.execute()
+
+    return solution
+}
+fun solution2 (): AoC_2022_02 {
+    val solution = AoC_2022_02("src/main/kotlin/data/inputFile2")
 
     solution.execute()
 
--- src/test/kotlin/AoC_2022_01_Test.kt
+++ src/test/kotlin/AoC_2022_01_Test.kt
@@ -2,16 +2,34 @@ import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
 class AoC_2022_01_Test {
+    private val testFile = "src/test/kotlin/data/testData1"
+    private val solution = AoC_2022_01(testFile)
+
+    fun setup() {
+        this.solution.execute()
+    }
 
     @Test
     fun givenTestData_whenExecute_thenCorrectMaxReturned() {
-        val testFile = "src/test/kotlin/data/testData1"
-        val expected = 24000
-        val solution = AoC_2022_01(testFile)
+        this.setup()
 
-        solution.execute()
-        val max = solution.maxTotal()
+        val expected = 24000
+
+        val max = this.solution.getMaxTotal()
 
         assertEquals(expected, max)
+    }
+
+    @Test
+    fun givenTestData_whenExecute_thenCorrectTopThreeReturned() {
+        setup()
+
+        val expected = listOf(24000, 24000, 16000)
+
+        solution.execute()
+        val topThree = solution.getTopThreeTotals()
+
+        assertEquals(expected.size, topThree.size)
+        assert(expected == topThree)
     }
 }
