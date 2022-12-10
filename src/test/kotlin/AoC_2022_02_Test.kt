e61dbaf0e91346b88a7be4b9790a59513877e3a6
Radu Adobroaie <radu.adobroaie@gmail.com>
2022-12-10 17:27:12 +0000

    Update problem 2 test for part 2

--- src/test/kotlin/AoC_2022_02_Test.kt
+++ src/test/kotlin/AoC_2022_02_Test.kt
@@ -2,15 +2,28 @@ import junit.framework.TestCase
 import org.junit.Test
 
 class AoC_2022_02_Test {
+    private val testFile = "src/test/kotlin/data/testData2"
+    private val solution = AoC_2022_02(testFile)
 
+    fun setup() {
+        this.solution.execute()
+    }
     @Test
     fun givenTestData_whenExecute_thenCorrectScoreReturned() {
-        val testFile = "src/test/kotlin/data/testData2"
-        val expected = 15
-        val solution = AoC_2022_02(testFile)
+        setup()
 
-        solution.execute()
-        val score = solution.getFinalScore()
+        val expected = 15
+        val score = this.solution.getFinalScore()
+
+        TestCase.assertEquals(expected, score)
+    }
+
+    @Test
+    fun givenTestData_whenExecute_thenCorrectSecondScoreReturned() {
+        setup()
+
+        val expected = 15
+        val score = this.solution.getSecondScore()
 
         TestCase.assertEquals(expected, score)
     }
